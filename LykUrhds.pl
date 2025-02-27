b38bb2439c73fefddbfd361c9d4faedbcaa56a6d
Romildo Júnior <139826286+Boudenzin@users.noreply.github.com>
2025-02-27 10:08:09 -0300

    Segunda Versão do Arquivo em Prolog

---
+++ LykUrhds.pl
@@ -0,0 +1,17 @@
+%Início do Programa das 8 Rainhas
+
+oito_rainhas :-
+    % Gera valores para as colunas das rainhas (1 a 8).
+    
+    Rainha1 in 1..8,
+    Rainha2 in 1..8,
+    Rainha3 in 1..8,
+    Rainha4 in 1..8,
+    Rainha5 in 1..8,
+    Rainha6 in 1..8,
+    Rainha7 in 1..8,
+    Rainha8 in 1..8,
+    
+    
+    
+    %Criar a regra de não ataque
